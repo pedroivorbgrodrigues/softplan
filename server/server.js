95cf092fb77f836f7f6ae25cb12732bcb3803472
pedroivorbgrodrigues <pedroivorbgrodrigues@gmail.com>
2018-11-23 19:39:55 -0200

    Fixed docker-compose and docker files

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const path = require('path');
 const cors = require('cors');
 const api = require('./routes/api');
 
@@ -18,7 +17,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', api);
-app.use(express.static(path.join(__dirname, 'static')));
 app.use((req, res) => {
   res.status(404).json({ message: 'You lost mate?' });
 });
